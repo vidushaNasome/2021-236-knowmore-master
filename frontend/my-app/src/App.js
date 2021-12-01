870ef410c4162403f6293afa7ae692df1573fb47
vidushaNasome <vnasome58@gmail.com>
2021-12-01 11:13:32 +0530

    check 01

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,10 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Page from "./Example/Page";
-import Page1 from "./Example/Page1";
-
-import Page2 from "./Page2";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import PrimarySearchAppBar from "./navigation/PrimarySearchAppBar";
 import Footer from "./navigation/Footer";
@@ -22,9 +17,6 @@ function App() {
         <LogginCheckPage/>
         <br/>
         <br/>
-        {/*<Footer/>*/}
-        {/*checking comment 3*/}
-        {/*checking comment 4*/}
 
     </div>
   );
